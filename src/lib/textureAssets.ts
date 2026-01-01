78ad46a5a69651d1600556e041b2dd949766bbbe
Ishan @ AI Workspace <ishandaga82@gmail.com>
2026-01-01 15:31:32 +0530

    feat: Changed the UI to allow user to choose from preuploaded textures

---
+++ src/lib/textureAssets.ts
@@ -0,0 +1,34 @@
+const textureAssets = [
+    {
+        id: 1,
+        name: 'Solid White',
+        path: '/assets/textures/solid-white.jpg'
+    },
+    {
+        id: 2,
+        name: 'Solid Red',
+        path: '/assets/textures/solid-red.jpg'
+    },
+    {
+        id: 3,
+        name: 'Mosaicum Coal',
+        path: '/assets/textures/mosaicum-coal-repeated-patterns-coordonne.jpg'
+    },
+    {
+        id: 4,
+        name: 'Marmorea Coal',
+        path: '/assets/textures/marmorea-coal-repeated-patterns-coordonne.jpg'
+    },
+    {
+        id: 5,
+        name: 'Maiorum Clay',
+        path: '/assets/textures/maiorum-clay-repeated-patterns-coordonne.jpg'
+    },
+    {
+        id: 6,
+        name: 'Olivetum Clay',
+        path: '/assets/textures/olivetum-clay-repeated-patterns-coordonne.jpg'
+    }
+]
+
+export default textureAssets;
